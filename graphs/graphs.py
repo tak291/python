b0c48b17af32b953c2d4ed8f7da860918c8fe6ef
jose <joseporras291@gmail.com>
2021-01-14 23:16:41 -0500

    Took csv apart

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -1,6 +1,15 @@
 import pandas as pd 
 
-df = pd.read_csv('/home/kike/Programming/python/graphs/assignment.csv',header=None, usecols=[1,2,3])
+df = pd.read_csv('/home/kike/Programming/python/graphs/graphs/assignment.csv',header=None, skiprows=1)
 
-print (df.head(10))
 
+#Printing the first 10 columns
+#print (df.head(1))
+
+
+location = df[0]
+dates = df[1]
+orders = df[2]
+percentage = df[3]
+
+print(location)
